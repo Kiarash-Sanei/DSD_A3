fcc6e6e11c2f03eac1a3c67713120ed44e0e4be8
Kiarash Sanei <kiarashsanei5@gmail.com>
2025-07-18 13:47:44 +0330

    Refactor module A to remove parameterization and simplify instantiation in CSA; delete unused module N.

--- Summation/A.v
+++ Summation/A.v
@@ -1,13 +1,11 @@
-module A #(parameter N = 4) (input [N-1:0] A, input [N-1:0] B, input C_in,
-            output C_out, output [N-1:0] S);
-    wire [N:0] C;
+module A (input [3:0] A, input [3:0] B, input C_in,
+            output C_out, output [3:0] S);
+    wire [4:0] C;
     assign C[0] = C_in;
-    assign C_out = C[N];
+    assign C_out = C[4];
 
-    genvar i;
-    generate
-        for (i = 0; i < N; i = i + 1) begin
-            FA fa (.A(A[i]), .B(B[i]), .C_in(C[i]), .C_out(C[i+1]), .S(S[i]));
-        end
-    endgenerate
+    FA fa_0(.A(A[0]), .B(B[0]), .C_in(C[0]), .C_out(C[1]), .S(S[0]));
+    FA fa_1(.A(A[1]), .B(B[1]), .C_in(C[1]), .C_out(C[2]), .S(S[1]));
+    FA fa_2(.A(A[2]), .B(B[2]), .C_in(C[2]), .C_out(C[3]), .S(S[2]));
+    FA fa_3(.A(A[3]), .B(B[3]), .C_in(C[3]), .C_out(C[4]), .S(S[3]));
 endmodule
--- Summation/N.v
+++
@@ -1,9 +0,0 @@
-module N #(parameter N = 16) (input [N-1:0] A,
-            output [N-1:0] B);
-    genvar i;
-    generate
-        for (i = 0; i < N; i = i + 1) begin
-            not n(B[i], A[i]);
-        end
-    endgenerate
-endmodule
